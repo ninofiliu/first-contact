9aa24af1ae72e6c73e754a8365764b5ea1a68f16
Nino Filiu <nino.filiu@gmail.com>
2023-06-01 17:38:29 +0200

    interactive home

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,29 @@ import { debug } from "./debug";
 import { live } from "./live";
 import { tv } from "./tv";
 
-const PROGRAM = "tv" as "tv" | "live" | "debug";
-
-if (PROGRAM === "tv") tv();
-if (PROGRAM === "live") live();
-if (PROGRAM === "debug") debug();
+const sp = new URLSearchParams(location.search);
+switch (sp.get("program")) {
+  case "tv":
+    tv();
+    break;
+  case "live":
+    live();
+    break;
+  case "debug":
+    debug();
+    break;
+  default: {
+    const ul = document.createElement("ul");
+    for (const programName of ["tv", "live", "debug"]) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const url = new URL(location.href);
+      url.searchParams.set("program", programName);
+      a.href = url.href;
+      a.innerHTML = programName;
+      li.append(a);
+      ul.append(li);
+    }
+    document.body.append(ul);
+  }
+}
